16e6d4373a44e814a98aaaca563ae0ea50c75329
joyrav09 <56099222+joyrav09@users.noreply.github.com>
2021-11-12 14:01:58 +0700

    Update soal2.php

--- soal2.php
+++ soal2.php
@@ -8,30 +8,30 @@
 <body>
 
 <?php
-function bubble_Sort($my_array ){
+function bubble_Sort($array ){
     do{
         $swapped = false;
-        for( $i = 0, $c = count( $my_array ) - 1; $i < $c; $i++ ){
-            if( $my_array[$i] > $my_array[$i + 1] ){
-                list( $my_array[$i + 1], $my_array[$i] ) =
-                        array( $my_array[$i], $my_array[$i + 1] );
+        for( $i = 0, $c = count( $array ) - 1; $i < $c; $i++ ){
+            if( $array[$i] > $array[$i + 1] ){
+                list( $array[$i + 1], $array[$i] ) =
+                        array( $array[$i], $array[$i + 1] );
                 $swapped = true;
             }
         }
     }
     while( $swapped );
-return $my_array;
+return $array;
 }
 
-$test_array = array('larine', 'aduh', 'qifuat', 'toda', 'anevi', 'samid', 'kifuat', 'jeom', 'lama', 'kamdo');
+$nama_string = array('larine', 'aduh', 'qifuat', 'toda', 'anevi', 'samid', 'kifuat', 'jeom', 'lama', 'kamdo');
 
 
 echo "Data Sebelum Diurutkan :<br>";
-echo implode(', ',$test_array );
+echo implode(', ',$nama_string );
 
 echo "<p></p>";
 echo "Data Setelah Diurutkan : <br>";
-echo implode(', ',bubble_Sort($test_array));
+echo implode(', ',bubble_Sort($nama_string));
 ?>
 
 </body>
